8ef3fcb9d28d2eeedcad631b69992ea886414d93
wesley-kami <wesleykami256@gmail.com>
2025-11-17 07:56:56 +0100

    Base project

---
+++ src/class/Data_Call.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+class Data_Call:
+    
+    def __init__(self):
+        pass
+    
+    def weatherCall(self):
+        pass
+    
